c0b6060619633f648f2840f3e152042076534fb1
TheOddCell <rayfb.to.1@gmail.com>
2026-06-06 19:25:43 -0400

    localing

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,9 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 SRC="$SCRIPT_DIR/.."
 
-install -Dm755 "$SRC/probed"         /usr/local/bin/probed
-install -Dm755 "$SRC/getinfo.sh"     /var/lib/probed/getinfo.sh
-install -Dm644 "$SRC/probed.service" /etc/systemd/system/probed.service
+install -Dm755 "$SRC/probed"               /usr/local/bin/probed
+install -Dm755 "$SRC/getinfo.sh"           /var/lib/probed/getinfo.sh
+install -Dm644 "$SRC/probed.service.local" /etc/systemd/system/probed.service
 if [ ! -f /etc/probed.conf ]; then
     install -Dm644 "$SRC/probed.conf" /etc/probed.conf
 fi
